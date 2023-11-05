1d4300164dff3a8d130bd50b46a0c48d881b13d2
BrunoVGGomes <bvgomes82@gmail.com>
2023-11-05 21:59:32 +0000

    Add reset

--- code.js
+++ code.js
@@ -3,20 +3,46 @@ class Counter {
       this.elInput = document.querySelector('#total-input');
       this.elAdd = document.querySelector('#add-value');
       this.elSub = document.querySelector('#sub-value');
+      this.resetButton = document.querySelector('#reset-button');
       this.total = 0;
   
       console.log('ElInput =', this.elInput, this.total);
   
       if (this.elAdd) {
         this.elAdd.addEventListener('click', () => {
-          this.total += 1;
+          console.log('elAdd =', this.elAdd);
+          this.total++;
           this.updateTotal();
         });
       }
   
       if (this.elSub) {
         this.elSub.addEventListener('click', () => {
-          this.total -= 1;
+          console.log('ElSub =', this.elSub);
+          this.total--;
+          this.updateTotal();
+        });
+      }
+
+      if (this.elInput) {
+        this.elInput.addEventListener('change', () => {
+          const newValue = Number(this.elInput.value);
+          console.log('elInput', newValue);
+
+          if (isNaN(newValue)) {
+            this.total = 0;
+            this.updateTotal();
+            return;
+          }
+
+          this.total = Number(this.elInput.value);
+          this.updateTotal();
+        });
+      }
+
+      if (this.resetButton) {
+        this.resetButton.addEventListener('click', () => {
+          this.total = 0;
           this.updateTotal();
         });
       }
